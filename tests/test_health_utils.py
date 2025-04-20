8bef55117aebf7dac853fc1803f9b4a3870c5d24
Senbonzakura95 <sosodu95140@gmail.com>
2025-04-21 01:57:57 +0200

    premier version du projet

--- tests/test_health_utils.py
+++ tests/test_health_utils.py
@@ -1,34 +1,19 @@
-import unittest
-from health_utils import calculate_bmi, calculate_bmr
+def test_calculate_bmr_male(self):
+    """Test BMR calculation for males"""
+    height = 175  # cm
+    weight = 70   # kg
+    age = 30      # années
+    gender = 'male'
+    expected_bmr = 1695.67  # Modifié
+    result = calculate_bmr(height, weight, age, gender)
+    self.assertAlmostEqual(result, expected_bmr, places=2)
 
-class TestHealthUtils(unittest.TestCase):
-    def test_calculate_bmi(self):
-        """Test BMI calculation"""
-        height = 1.75  # mètres
-        weight = 70    # kg
-        expected_bmi = 22.86
-        result = calculate_bmi(height, weight)
-        self.assertAlmostEqual(result, expected_bmi, places=2)
-
-    def test_calculate_bmr_male(self):
-        """Test BMR calculation for males"""
-        height = 175  # cm
-        weight = 70   # kg
-        age = 30      # années
-        gender = 'male'
-        expected_bmr = 1723.46
-        result = calculate_bmr(height, weight, age, gender)
-        self.assertAlmostEqual(result, expected_bmr, places=2)
-
-    def test_calculate_bmr_female(self):
-        """Test BMR calculation for females"""
-        height = 175  # cm
-        weight = 70   # kg
-        age = 30      # années
-        gender = 'female'
-        expected_bmr = 1432.79
-        result = calculate_bmr(height, weight, age, gender)
-        self.assertAlmostEqual(result, expected_bmr, places=2)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_calculate_bmr_female(self):
+    """Test BMR calculation for females"""
+    height = 175  # cm
+    weight = 70   # kg
+    age = 30      # années
+    gender = 'female'
+    expected_bmr = 1507.13  # Modifié
+    result = calculate_bmr(height, weight, age, gender)
+    self.assertAlmostEqual(result, expected_bmr, places=2)
