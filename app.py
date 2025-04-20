488bcff44d7ddeee355884d03a17e7cb98b358a0
Senbonzakura95 <sosodu95140@gmail.com>
2025-04-21 01:42:23 +0200

    premier version du projet

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from health_utils import calculate_bmi, calculate_bmr
+
+app = Flask(__name__)
+
+@app.route('/bmi', methods=['POST'])
+def bmi():
+    data = request.get_json()
+    height = data['height']
+    weight = data['weight']
+    bmi = calculate_bmi(height, weight)
+    return jsonify({'bmi': bmi})
+
+@app.route('/bmr', methods=['POST'])
+def bmr():
+    data = request.get_json()
+    height = data['height']
+    weight = data['weight']
+    age = data['age']
+    gender = data['gender']
+    bmr = calculate_bmr(height, weight, age, gender)
+    return jsonify({'bmr': bmr})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
